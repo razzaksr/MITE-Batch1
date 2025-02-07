b1705ba74c357e79a95f74b1b376f62759b2af7e
razzaksr <razzaksr@gmail.com>
2025-02-07 10:52:55 +0530

    introductions

---
+++ Day1/Macro.java
@@ -0,0 +1,16 @@
+package Day1;
+
+class Macro{
+
+}
+
+class Micro{
+    public static void main(String[] args) {
+        System.out.println("Welcome to DLithe");
+    }
+}
+class Fiber{
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Development");
+    }
+}
